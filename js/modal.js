1843611d5acb02ef5708c4011112ed964a2606c0
nnunun08 <52190553+nnunun08@users.noreply.github.com>
2023-01-19 08:38:25 +0900

    update

--- js/modal.js
+++ js/modal.js
@@ -5,14 +5,25 @@ const body = document.querySelector('body')
 for(i = 0; i < trigger.length; i++) {
     trigger[i].addEventListener('click', (e) => {
         modal.classList.add('modal')
-        modalItem[0].classList.add('on')
+        let target = e.currentTarget.attributes[1].nodeValue;
+        console.log(target)
+        for(j = 0; j < modalItem.length; j++) {
+            if(modalItem[j].attributes[1].value === target) {
+                let modalOpen = modalItem[j]
+                modalOpen.classList.add('on')
+            }
+        }
+        console.log()
+        // modalItem[0].classList.add('on')
         body.style.overflow = 'hidden'
     })
 }
 
 function modalClose() {
     modal.classList.remove('modal')
-    modalItem[0].classList.remove('on')
+    for(i=0;i<modalItem.length;i++) {
+        modalItem[i].classList.remove('on')
+    }
     body.style.overflow = '';
 }
 
