bdc91575ba8f485142a151f757ac15e18f5e5538
nnunun08 <52190553+nnunun08@users.noreply.github.com>
2023-02-20 01:04:44 +0900

    interaction++

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,10 @@ const trigger = document.querySelectorAll('.contents .item');
 const modal = document.querySelector('.detail');
 const modalItem = document.querySelectorAll('.detail .item');
 const body = document.querySelector('body');
+const footer = document.querySelector('footer');
+const mail = document.querySelector('.mail');
+const leftCon = document.querySelectorAll('.home .detail .item .left');
+console.log(leftCon);
 for (i = 0; i < trigger.length; i++) {
     trigger[i].addEventListener('click', (e) => {
         modal.classList.add('modal');
@@ -11,20 +15,38 @@ for (i = 0; i < trigger.length; i++) {
         for (j = 0; j < modalItem.length; j++) {
             if (modalItem[j].attributes[1].value === target) {
                 let modalOpen = modalItem[j];
+                let leftConTarget = leftCon[j];
+                // const leftCon = modalOpen.querySelector('.left')
                 modalOpen.classList.add('on');
+                setTimeout(function () {
+                    modalOpen.style.opacity = '1';
+                    modalOpen.style.transform = 'translate(-50%, -50%)';
+                    leftConTarget.style.opacity = '1';
+                    leftConTarget.style.transform = 'translateY(0)';
+                }, 200);
             }
         }
         console.log();
         // modalItem[0].classList.add('on')
         body.style.overflow = 'hidden';
+        footer.style.transform = 'translateY(120px)';
+        mail.style.opacity = '0';
+        mail.style.right = '-80px';
     });
 }
 function modalClose() {
     modal.classList.remove('modal');
     for (i = 0; i < modalItem.length; i++) {
         modalItem[i].classList.remove('on');
+        modalItem[i].style.opacity = null;
+        modalItem[i].style.transform = null;
+        leftCon[i].style.opacity = null;
+        leftCon[i].style.transform = null;
     }
     body.style.overflow = '';
+    footer.style.transform = null;
+    mail.style.opacity = null;
+    mail.style.right = null;
 }
 modal.addEventListener('click', (e) => {
     if (e.target.className === 'detail modal') {
--- js/modal.ts
+++ js/modal.ts
@@ -2,6 +2,10 @@ const trigger = document.querySelectorAll('.contents .item')
 const modal:any = document.querySelector('.detail')
 const modalItem = document.querySelectorAll('.detail .item')
 const body:any = document.querySelector('body')
+const footer:any = document.querySelector('footer')
+const mail:any = document.querySelector('.mail')
+const leftCon:any = document.querySelectorAll('.home .detail .item .left')
+console.log(leftCon)
 for(i = 0; i < trigger.length; i++) {
     trigger[i].addEventListener('click', (e:any) => {
         modal.classList.add('modal')
@@ -10,12 +14,23 @@ for(i = 0; i < trigger.length; i++) {
         for(j = 0; j < modalItem.length; j++) {
             if(modalItem[j].attributes[1].value === target) {
                 let modalOpen = modalItem[j]
+                let leftConTarget = leftCon[j]
+                // const leftCon = modalOpen.querySelector('.left')
                 modalOpen.classList.add('on')
+                setTimeout(function(){
+                    modalOpen.style.opacity = '1'
+                    modalOpen.style.transform = 'translate(-50%, -50%)'
+                    leftConTarget.style.opacity = '1'
+                    leftConTarget.style.transform = 'translateY(0)'
+                }, 200)
             }
         }
         console.log()
         // modalItem[0].classList.add('on')
-        body.style.overflow = 'hidden'
+        body.style.overflow = 'hidden';
+        footer.style.transform = 'translateY(120px)';
+        mail.style.opacity = '0';
+        mail.style.right = '-80px';
     })
 }
 
@@ -23,8 +38,15 @@ function modalClose() {
     modal.classList.remove('modal')
     for(i=0;i<modalItem.length;i++) {
         modalItem[i].classList.remove('on')
+        modalItem[i].style.opacity = null;
+        modalItem[i].style.transform = null;
+        leftCon[i].style.opacity = null;
+        leftCon[i].style.transform = null;
     }
     body.style.overflow = '';
+    footer.style.transform = null;
+    mail.style.opacity = null;
+    mail.style.right = null;
 }
 
 
